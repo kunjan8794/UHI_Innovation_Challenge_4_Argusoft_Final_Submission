4e4d62542f29305c34dd803ff5d5aef28440c8a4
Namrata Dodiya <ndodiya@argusoft.in>
2022-07-15 13:00:23 +0530

    Fix the flow master dao changes

--- abdm_hackathon/src/main/java/com/argusoft/abdmhackathon/flow/dao/impl/FlowMasterDaoImpl.java
+++ abdm_hackathon/src/main/java/com/argusoft/abdmhackathon/flow/dao/impl/FlowMasterDaoImpl.java
@@ -6,6 +6,7 @@ import com.argusoft.abdmhackathon.flow.dao.FlowMasterCustomDao;
 import com.argusoft.abdmhackathon.flow.model.FlowMaster;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -17,9 +18,8 @@ import java.util.List;
 public class FlowMasterDaoImpl extends GenericRepositoryImpl implements FlowMasterCustomDao {
 
 
-
     @Override
-    public Integer getFirstQuestionId () {
+    public Integer getFirstQuestionId() {
         Session session = getSession();
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<FlowMaster> criteria = criteriaBuilder.createQuery(FlowMaster.class);
@@ -29,26 +29,30 @@ public class FlowMasterDaoImpl extends GenericRepositoryImpl implements FlowMast
         List<FlowMaster> flowMasters = (List<FlowMaster>) session.createQuery(criteria).list();
         return flowMasters.get(0).getQuestionId();
     }
+
     @Override
-    public Integer getFlowByQuestionIDAndAnswer(Integer questionId,String answer){
+    public Integer getFlowByQuestionIDAndAnswer(Integer questionId, String answer) {
         Session session = getSession();
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<FlowMaster> criteria = criteriaBuilder.createQuery(FlowMaster.class);
         Root<FlowMaster> root = criteria.from(FlowMaster.class);
-        Predicate questionCondition =null;
-        if(questionId != null && !answer.equals(null) ){
+        Predicate questionCondition = null;
+
+        if (questionId != null && answer != null) {
             // if questionId is there and answer is not null
             questionCondition = criteriaBuilder.equal(root.get(FlowMaster.Fields.QUESTION_ID), questionId);
             Predicate answerCondition = criteriaBuilder.equal(root.get(FlowMaster.Fields.ANSWER), answer);
             criteria.select(root).where(criteriaBuilder.and(questionCondition, answerCondition));
-            FlowMaster flowMaster=(FlowMaster) session.createQuery(criteria).list().get(0);
-            return flowMaster.getNextQuestionId();
+            List<FlowMaster> flowMaster = session.createQuery(criteria).list();
+            if (CollectionUtils.isEmpty(flowMaster))
+                return null;
+            return flowMaster.get(0).getNextQuestionId();
         } else {
             questionCondition = criteriaBuilder.equal(root.get(FlowMaster.Fields.QUESTION_ID), questionId);
         }
         criteria.select(root).where(questionCondition);
-        FlowMaster flowMaster=(FlowMaster) session.createQuery(criteria);
+        FlowMaster flowMaster = (FlowMaster) session.createQuery(criteria).uniqueResult();
         return flowMaster.getNextQuestionId();
-    };
+    }
 
 }
