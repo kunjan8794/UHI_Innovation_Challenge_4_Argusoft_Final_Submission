8b339b388734f288437d3859c917494ceb7e5478
Namrata Dodiya <ndodiya@argusoft.in>
2022-07-16 12:22:54 +0530

    Added lab tests

---
+++ abdm_hackathon/src/main/java/com/argusoft/abdmhackathon/labtest/dao/impl/LabTestDaoImpl.java
@@ -0,0 +1,46 @@
+package com.argusoft.abdmhackathon.labtest.dao.impl;
+
+import com.argusoft.abdmhackathon.flow.model.FlowMaster;
+import com.argusoft.abdmhackathon.labtest.dao.LabTestCustomDao;
+import com.argusoft.abdmhackathon.labtest.model.LabTest;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+/**
+ * <p>
+ * Add description here
+ * </p>
+ *
+ * @author namrata
+ * @since 16/07/2022 11:33 AM
+ */
+@Repository
+public class LabTestDaoImpl implements LabTestCustomDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Session getSession() {
+        return entityManager.unwrap(Session.class);
+    }
+
+    @Override
+    public List<LabTest> getAllByCode(String code) {
+        Session session = getSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<LabTest> criteria = criteriaBuilder.createQuery(LabTest.class);
+        Root<LabTest> root = criteria.from(LabTest.class);
+        Predicate condition = criteriaBuilder.equal(root.get(LabTest.Fields.CODE), code);
+        criteria.select(root).where(condition);
+        List<LabTest> labTests = session.createQuery(criteria).list();
+        return labTests;
+    }
+}
--- abdm_hackathon/src/main/java/com/argusoft/abdmhackathon/mobile/controller/MobileController.java
+++ abdm_hackathon/src/main/java/com/argusoft/abdmhackathon/mobile/controller/MobileController.java
@@ -1,13 +1,12 @@
 package com.argusoft.abdmhackathon.mobile.controller;
 
+import com.argusoft.abdmhackathon.labtest.service.LabTestService;
 import com.argusoft.abdmhackathon.mobile.dto.TriagingAndFormDataDto;
 import com.argusoft.abdmhackathon.triage.TriagingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,6 +15,14 @@ public class MobileController {
 
     @Autowired
     TriagingService triagingService;
+
+    @Autowired
+    private LabTestService labTestService;
+
+    @RequestMapping(value = "/lab-tests-by-codes", method = RequestMethod.GET)
+    public Map<String, List<String>> getLabTestsByCodes(@RequestParam String codes) {
+        return labTestService.getLabTestsByCodes(codes);
+    }
 
     @RequestMapping(value = "submit-answers", method = RequestMethod.POST)
     public String submitAnswers(@RequestBody Map<Integer, String> submitData) {
