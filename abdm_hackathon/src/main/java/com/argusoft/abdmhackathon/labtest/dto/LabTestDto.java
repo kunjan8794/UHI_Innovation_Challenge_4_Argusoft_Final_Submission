a5b8f9200be0c18363dd0c3551706677de74775b
Namrata Dodiya <ndodiya@argusoft.in>
2022-07-16 18:43:34 +0530

    chnage in get lab tests api

---
+++ abdm_hackathon/src/main/java/com/argusoft/abdmhackathon/labtest/dto/LabTestDto.java
@@ -0,0 +1,30 @@
+package com.argusoft.abdmhackathon.labtest.dto;
+
+/**
+ * <p>
+ * Add description here
+ * </p>
+ *
+ * @author namrata
+ * @since 16/07/2022 6:30 PM
+ */
+public class LabTestDto {
+    private String name;
+    private Integer code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
